8e60e8fb1b8075e11a2304a076273db21f7133dc
IPleshivtsev <ilya.pleshivtsev@yandex.ru>
2020-11-23 22:14:04 +0300

    5.3 Map Reduce React

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,37 @@ import { Layout } from './shared/Layout';
 import {Header} from "./shared/Header";
 import {Content} from "./shared/Content";
 import {CardsList} from "./shared/CardsList";
+import {assignId, generateId, generateRandomString} from "./utils/react/generateRandomIndex";
+import {merge} from "./utils/js/merge";
+import {MyList} from "./shared/GenericList/GenericList";
 
-export function AppComponent() {
+const LIST = [
+    {value: 'some'},
+    {value: 'other some'},
+    {value: 'some'}
+].map(generateId)
+
+function AppComponent() {
+    const [list, setList] = React.useState(LIST);
+
+    const handleItemClick = (id: string) => {
+        setList(list.filter((item) => item.id != id));
+    }
+
+    const handleAdd = () => {
+        setList(list.concat(generateId({value: generateRandomString()})))
+    }
+
     return (
         <Layout>
             <Header />
             <Content>
                 <CardsList />
+                <button onClick={handleAdd}>Add Element</button>
+                <MyList list={LIST.map(merge({onClick: handleItemClick}))} />
             </Content>
         </Layout>
     );
 }
 
-export const App = hot(AppComponent);
+export const App = hot(() => <AppComponent />);
