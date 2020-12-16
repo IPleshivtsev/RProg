b3ebda70b08ebbab3ba441f44f006c20d31bde52
IPleshivtsev <ilya.pleshivtsev@yandex.ru>
2020-12-16 21:35:19 +0300

    6.4 Пишем тест на реакт компонент Enzyme vs Snapshots

---
+++ src/shared/Dropdown/__tests__/Dropdown.test.tsx
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from "react";
+import { shallow } from 'enzyme';
+import { Dropdown } from '../Dropdown';
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+describe('Dropdown', () => {
+    test('should render', () => {
+        const wrapper = shallow(<Dropdown children={<div />} button={<button />} />)
+        expect(wrapper).toBeDefined();
+        expect(wrapper.find('div.container').isEmptyRender()).toBeFalsy();
+    })
+
+    test('should render (snapshot)', () => {
+        const wrapper = shallow(<Dropdown children={<div />} button={<button />} />)
+
+        expect(wrapper).toMatchSnapshot();
+    })
+})
