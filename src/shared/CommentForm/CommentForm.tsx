109ef56a04fd6b1a0e6d780e5d3c04ceee53912b
IPleshivtsev <ilya.pleshivtsev@yandex.ru>
2021-02-10 20:18:54 +0300

    Effector

--- src/shared/CommentForm/CommentForm.tsx
+++ src/shared/CommentForm/CommentForm.tsx
@@ -1,42 +1,56 @@
-import React from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
 import styles from './commentform.css';
 import {useFormik} from 'formik';
 import {useDispatch} from "react-redux";
-import {updateComment} from "../../store/reducer";
+import {createEvent, createStore} from "effector";
+import { useStore } from 'effector-react';
+
+const updateComment = createEvent<string>();
+
+const $comment = createStore('Привет из Effector')
+    .on(updateComment, (_, newValue) => newValue);
+
+$comment.watch((state) => {
+    console.log('state', state);
+})
 
 export function CommentForm() {
-    const dispatch = useDispatch();
+    const value = useStore($comment);
 
-    const formik = useFormik({
-        initialValues: {
-            comment: ''
-        },
-        validate: values => {
-            const errors = {} as {comment?: string};
-            if (!values.comment) {
-                errors.comment = 'Поле обязательно для заполнения';
-            } else if (values.comment.length <= 3) {
-                errors.comment = 'Введите больше 3-х символов';
-            }
-            return errors;
-        },
-        onSubmit: values => {
-            dispatch(updateComment(values.comment));
-            alert('Форма отправлена!');
-        }
-    });
+    const [touched, setTouched] = useState(false);
+    const [valueError, setValueError] = useState('');
+
+    function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+        setTouched(true);
+        setValueError(validateValue());
+
+        const isFormValid = !validateValue();
+        if (!isFormValid) return;
+
+        alert('Форма отправлена!');
+    }
+
+    function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
+        updateComment(event.target.value);
+    }
+
+    function validateValue() {
+        if (value.length <= 3) return 'Введите больше 3-х символов';
+        return '';
+    }
 
   return (
-      <form className={styles.form} onSubmit={formik.handleSubmit} >
+      <form className={styles.form} onSubmit={handleSubmit} >
           <textarea
               id="comment"
               name="comment"
               className={styles.input}
-              onChange={formik.handleChange}
-              value={formik.values.comment}
-              aria-invalid={formik.errors.comment ? 'true' : undefined}
+              onChange={handleChange}
+              value={value}
+              aria-invalid={valueError ? 'true' : undefined}
           />
-          {formik.touched.comment && formik.errors.comment && (<div>{formik.errors.comment}</div>)}
+          {touched && valueError && (<div>{valueError}</div>)}
           <button type="submit" className={styles.button}>Комментировать</button>
       </form>
   );
